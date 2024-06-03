2ea2f68516c098c43a860cf74433a2a3449b3e6b
purboyndra <purboindra@gmail.com>
2024-06-03 20:07:04 +0700

    hidden navbar smal device

--- components/shared/Navbar.tsx
+++ components/shared/Navbar.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 export const Navbar = () => {
   return (
-    <nav className="flex justify-between bg-neutral-100 fixed z-50 w-full p-6 shadow">
+    <nav className="md:flex justify-between bg-neutral-100 fixed z-50 w-full p-6 shadow hidden">
       <div className="flex justify-between">
         <h1 className="text-lg font-semibold text-neutral-700">Logo</h1>
       </div>
