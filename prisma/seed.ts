9756fe5943329b5a7789b4b8feb7c9a74b804041
purboyndra <purboindra@gmail.com>
2024-06-06 19:56:25 +0700

    seeding prisma

---
+++ prisma/seed.ts
@@ -0,0 +1,34 @@
+import prisma from "../lib/prisma";
+// run inside `async` function
+async function main() {
+  const alice = await prisma.user.upsert({
+    where: { email: "alice@prisma.io" },
+    update: {},
+    create: {
+      email: "alice@prisma.io",
+      username: "Alice",
+      password: "qwerty",
+      photo_url: "",
+    },
+  });
+  const bob = await prisma.user.upsert({
+    where: { email: "bob@prisma.io" },
+    update: {},
+    create: {
+      email: "bob@prisma.io",
+      username: "Bob",
+      password: "qwerty",
+      photo_url: "",
+    },
+  });
+  console.log({ alice, bob });
+}
+main()
+  .then(async () => {
+    await prisma.$disconnect();
+  })
+  .catch(async (e) => {
+    console.error(e);
+    await prisma.$disconnect();
+    process.exit(1);
+  });
