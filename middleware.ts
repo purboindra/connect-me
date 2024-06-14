4ad82daed6a89c4d2d6c91f2e5d40f43ec566ad1
purboyndra <purboindra@gmail.com>
2024-06-14 20:31:55 +0700

    temp comment middleware

--- middleware.ts
+++ middleware.ts
@@ -2,20 +2,20 @@ import { NextResponse, NextRequest } from "next/server";
 import { isTokenExpired } from "./lib/utils";
 
 export async function middleware(request: NextRequest) {
-  const token = request.cookies.get("access_token")?.value;
+  // const token = request.cookies.get("access_token")?.value;
 
-  const hasOnLogin = request.url.includes("/login");
-  const hasOnRegister = request.url.includes("/register");
+  // const hasOnLogin = request.url.includes("/login");
+  // const hasOnRegister = request.url.includes("/register");
 
-  if (!token && !hasOnLogin && !hasOnRegister) {
-    return NextResponse.redirect(new URL("/login", request.url));
-  }
+  // if (!token && !hasOnLogin && !hasOnRegister) {
+  //   return NextResponse.redirect(new URL("/login", request.url));
+  // }
 
-  const isExpired = isTokenExpired(token || "");
+  // const isExpired = isTokenExpired(token || "");
 
-  if (isExpired && !hasOnLogin && !hasOnRegister) {
-    return NextResponse.redirect(new URL("/login", request.url));
-  }
+  // if (isExpired && !hasOnLogin && !hasOnRegister) {
+  //   return NextResponse.redirect(new URL("/login", request.url));
+  // }
 
   return NextResponse.next();
 }
