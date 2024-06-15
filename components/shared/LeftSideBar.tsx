64a50880bf44a5a2d4c2af02a70f7060c250a429
purboyndra <purboindra@gmail.com>
2024-06-15 21:40:59 +0700

    update leftsidebar

--- components/shared/LeftSideBar.tsx
+++ components/shared/LeftSideBar.tsx
@@ -9,8 +9,11 @@ import React from "react";
 export const LeftSideBar = () => {
   const pathname = usePathname();
   return (
-    <section className="left-0 top-0 sticky flex justify-between flex-col h-screen overflow-y-auto border-r pl-2 pr-14 max-lg:pr-8 pt-28 pb-8 max-sm:hidden lg:w-[200px] ">
-      <div className="flex space-y-8 flex-col">
+    <section className="left-0 top-0 sticky flex justify-between flex-col h-screen overflow-y-auto pl-8 pr-14 max-lg:pr-8 pt-8 pb-8 max-sm:hidden lg:w-[220px]">
+      <div className="flex space-y-2 flex-col">
+        <h1 className="text-xl font-semibold text-neutral-800 mb-8">
+          Connect Me
+        </h1>
         {sidebarLinks.map((item, index) => {
           const isActive =
             (pathname.includes(item.route) && item.route.length > 1) ||
@@ -20,7 +23,9 @@ export const LeftSideBar = () => {
             <Link
               href={item.route}
               key={index}
-              className="flex flex-col items-start justify-center"
+              className={`flex flex-col items-start justify-center py-1 px-2 ${
+                isActive && "rounded bg-neutral-400/10"
+              }`}
             >
               <div className="flex flex-row items-center space-x-2">
                 <Image
@@ -33,33 +38,37 @@ export const LeftSideBar = () => {
                   }`}
                 />
                 <p
-                  className={`max-lg:hidden text-base font-semibold ${
+                  className={`max-lg:hidden text-base font-medium ${
                     isActive ? " text-neutral-700" : " text-neutral-700/40"
                   }`}
                 >
                   {item.label}
                 </p>
               </div>
-              <div
-                className={`h-2 w-6 flex items-start mt-1 lg:hidden ${
-                  isActive ? "bg-blue-500" : "bg-neutral-200"
-                }`}
-              />
             </Link>
           );
         })}
       </div>
       <div className="flex space-x-4 hover:cursor-pointer">
-        <Image
-          src={"/assets/icons/mingcute-down-line.svg"}
-          alt="Sidebar Icon"
-          width={20}
-          height={20}
-          className="invert"
-        />
-        <p className="text-base font-semibold text-neutral-700 max-lg:hidden">
-          More
-        </p>
+        <Link
+          href={"/profile"}
+          className={`flex flex-col items-start justify-center py-1 px-2 `}
+        >
+          <div className="flex flex-row items-center space-x-2">
+            <Image
+              src={"/assets/icons/user.svg"}
+              alt="Sidebar Icon"
+              width={20}
+              height={20}
+              className="text-neutral-900"
+            />
+            <p
+              className={`max-lg:hidden text-base font-medium  text-neutral-700`}
+            >
+              Profile
+            </p>
+          </div>
+        </Link>
       </div>
     </section>
   );
