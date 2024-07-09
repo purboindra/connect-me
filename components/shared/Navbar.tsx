4038131020f46e0137966738c765d5d65ce53a5d
purboyndra <purboindra@gmail.com>
2024-07-09 18:39:22 +0700

    sheet

--- components/shared/Navbar.tsx
+++ components/shared/Navbar.tsx
@@ -1,84 +1,14 @@
 "use client";
 
 import React from "react";
-import {
-  Sheet,
-  SheetClose,
-  SheetContent,
-  SheetTrigger,
-} from "@/components/ui/sheet";
+import { Sheet, SheetContent, SheetTrigger } from "@/components/ui/sheet";
 import Image from "next/image";
-import Link from "next/link";
-import { usePathname } from "next/navigation";
-import { sidebarLinks } from "@/constants";
+import { CustomSheetContent } from "./SheetContent";
 
 const NavContent = () => {
-  const pathname = usePathname();
   return (
     <section className="flex h-full flex-col gap-6 pt-8">
-      <SheetClose className="flex" asChild>
-        <Link href={"/"} className="hover:cursor-pointer">
-          <h1 className="text-xl font-semibold text-neutral-800 mb-8">
-            Connect Me
-          </h1>
-        </Link>
-      </SheetClose>
-      {sidebarLinks.map((item, index) => {
-        const isActive =
-          (pathname.includes(item.route) && item.route.length > 1) ||
-          pathname == item.route;
-
-        return (
-          <SheetClose key={item.route} asChild>
-            <Link
-              href={item.route}
-              className={`flex flex-col items-start justify-center py-1 px-2 ${
-                isActive && "rounded bg-neutral-400/10"
-              }`}
-            >
-              <div className="flex flex-row items-center space-x-2">
-                <Image
-                  src={item.imgUrl}
-                  alt="Sidebar Icon"
-                  width={20}
-                  height={20}
-                  className={` ${
-                    isActive ? "text-neutral-900" : "text-neutral-400"
-                  }`}
-                />
-                <p
-                  className={`text-base font-medium ${
-                    isActive ? " text-neutral-700" : " text-neutral-700/40"
-                  }`}
-                >
-                  {item.label}
-                </p>
-              </div>
-            </Link>
-          </SheetClose>
-        );
-      })}
-      <SheetClose className="flex h-full items-end  hover:cursor-pointer">
-        <div className="space-x-4">
-          <Link
-            href={"/profile"}
-            className={`flex flex-col items-start justify-center py-1 px-2 `}
-          >
-            <div className="flex flex-row items-center space-x-2">
-              <Image
-                src={"/assets/icons/user.svg"}
-                alt="Sidebar Icon"
-                width={20}
-                height={20}
-                className="text-neutral-900"
-              />
-              <p className={`text-base font-medium  text-neutral-700/40`}>
-                Profile
-              </p>
-            </div>
-          </Link>
-        </div>
-      </SheetClose>
+      <CustomSheetContent />
     </section>
   );
 };
@@ -87,7 +17,7 @@ export const Navbar = () => {
   return (
     <nav className="max-sm:flex justify-between bg-neutral-100 fixed z-50 w-full py-3 px-8 shadow hidden">
       <div className="flex w-full items-center justify-between">
-        <h1 className="text-lg font-semibold text-neutral-700">Logo</h1>
+        <h1 className="text-base font-bold text-neutral-700">Connect Me</h1>
         <div className="flex flex-row space-x-6 items-center">
           <Sheet>
             <SheetTrigger asChild>
