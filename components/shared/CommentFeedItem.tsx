5beae7239d6382c1f5aaaae57ab968fc358f6748
purboyndra <purboindra@gmail.com>
2024-07-16 06:01:26 +0700

    comment feed item

---
+++ components/shared/CommentFeedItem.tsx
@@ -0,0 +1,83 @@
+import { createComment } from "@/app/actions/post.action";
+import { CommentInterface } from "@/types";
+import React, { useOptimistic, useState, useTransition } from "react";
+
+interface CommentFeedItemInterface {
+  postId: string;
+  userId: string;
+  comments: Array<CommentInterface>;
+}
+
+export const CommentFeedItem = ({
+  postId,
+  userId,
+  comments,
+}: CommentFeedItemInterface) => {
+  const [isPending, startTransition] = useTransition();
+
+  const [newComment, setNewComment] = useState("");
+
+  const [optimisticMessages, addOptimisticMessage] = useOptimistic(
+    comments,
+    (state, newComments: CommentInterface) => [...state, newComments]
+  );
+
+  const handleClick = async (e: React.MouseEvent<HTMLButtonElement>) => {
+    e.preventDefault();
+
+    startTransition(() => {
+      addOptimisticMessage({
+        content: newComment,
+        createdAt: Date.now(),
+        postid: postId,
+        userId: userId,
+      });
+    });
+
+    await createComment(new FormData(e.currentTarget.form as HTMLFormElement));
+
+    setNewComment("");
+  };
+
+  return (
+    <div className="mt-1 flex flex-col gap-1 w-full">
+      {optimisticMessages.length < 2 && (
+        <>
+          {optimisticMessages.map((comment, index) => (
+            <p key={index}>{comment.content}</p>
+          ))}
+        </>
+      )}
+
+      {optimisticMessages.length > 1 && (
+        <h2 className="text-sm font-medium text-neutral-500 hover:cursor-pointer">
+          {`View all ${optimisticMessages.length} comments`}
+        </h2>
+      )}
+      <div className="flex gap-1">
+        <span className="text-sm font-normal text-neutral-600">Comment as</span>
+        <span>
+          <h2 className="text-sm font-semibold text-neutral-600">Purboyndra</h2>
+        </span>
+      </div>
+      <form className="flex w-full gap-1 justify-between items-center">
+        <input
+          placeholder="Write a comment..."
+          name="comment"
+          value={newComment}
+          onChange={(e) => setNewComment(e.target.value)}
+          type="text"
+          className="text-sm font-normal text-neutral-600 p-1 w-full focus:outline-none focus:border-none"
+        />
+        <input type="hidden" name="postId" value={postId} />
+
+        <button
+          className="text-xs font-medium text-blue-600"
+          onClick={handleClick}
+        >
+          Send
+        </button>
+      </form>
+    </div>
+  );
+};
