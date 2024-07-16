5ba0d3bfee737c83361cd21eab0244290e4182ee
purboyndra <purboindra@gmail.com>
2024-07-17 06:29:46 +0700

    error image

--- components/shared/PostItem.tsx
+++ components/shared/PostItem.tsx
@@ -1,5 +1,7 @@
+"use client";
+
 import { PostInterface } from "@/types";
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Image from "next/image";
 import { MoreHorizontal } from "lucide-react";
 import DateComponent from "./DateFeedItem";
@@ -9,6 +11,12 @@ interface PostItemInterface {
 }
 
 export const PostItem = ({ post }: PostItemInterface) => {
+  const [src, setSrc] = useState(post.imageUrl);
+
+  useEffect(() => {
+    setSrc(src);
+  }, [src]);
+
   return (
     <div className="flex flex-col gap-2 ">
       <div className="flex justify-between items-center">
@@ -52,6 +60,7 @@ export const PostItem = ({ post }: PostItemInterface) => {
             fill
             objectFit="cover"
             className="object-top"
+            onErrorCapture={(e) => setSrc("/assets/images/error.png")}
           />
         ) : (
           <div className="h-full w-full bg-neutral-300">
