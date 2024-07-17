6561f65d9358e5136d6eb3f06a550242097c9342
purboyndra <purboindra@gmail.com>
2024-07-18 05:43:15 +0700

    fix style postgrid

--- components/shared/PostGrid.tsx
+++ components/shared/PostGrid.tsx
@@ -8,9 +8,9 @@ interface PostGridInterface {
 
 const PostGrid = ({ posts }: PostGridInterface) => {
   return (
-    <div className="w-full grid grid-cols-3 gap-[1px] mt-2">
+    <div className="w-full grid max-sm:grid-cols-3 grid-cols-5 gap-[1px] mt-2">
       {posts.map((post) => (
-        <div key={post.id} className="h-[155px]">
+        <div key={post.id} className="max-sm:h-[155px] h-full">
           <Image
             src={post!.imageUrl || ""}
             alt="post"
