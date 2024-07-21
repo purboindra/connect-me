e87b7fda1fb1bc84309b4f891afb3992f1c83be4
purboyndra <purboindra@gmail.com>
2024-07-22 05:19:42 +0700

    if post is empty postgrid

--- components/shared/PostGrid.tsx
+++ components/shared/PostGrid.tsx
@@ -8,13 +8,24 @@ interface PostGridInterface {
 }
 
 const PostGrid = ({ posts }: PostGridInterface) => {
-  return (
+  return posts.length > 0 ? (
     <div className="w-full grid grid-cols-3 md:grid-cols-4 xl:grid-cols-5   gap-[1px] mt-2">
       {posts.map((post) => (
         <div key={post.id} className="max-sm:h-[155px] h-full">
           <ImageGrid imageUrl={post!.imageUrl || ""} />
         </div>
       ))}
+    </div>
+  ) : (
+    <div className="flex flex-col gap-4 h-48 items-center mx-auto justify-center">
+      <Image
+        src={"/assets/icons/video.svg"}
+        alt="video"
+        width={64}
+        height={64}
+        className=" text-neutral-400"
+      />
+      <h2 className="text-base font-semibold text-neutral-800">No post yet</h2>
     </div>
   );
 };
