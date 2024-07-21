72551a65a75a04e9ac762d77f59e0f6a27b4c789
purboyndra <purboindra@gmail.com>
2024-07-22 05:19:16 +0700

    if savedpost is empty

--- components/shared/SavedPosts.tsx
+++ components/shared/SavedPosts.tsx
@@ -7,14 +7,28 @@ interface SavedPostsInterface {
 }
 
 const SavedPosts = ({ posts }: SavedPostsInterface) => {
-  console.log(posts);
-  return (
+  return posts.length > 0 ? (
     <div className="w-full grid grid-cols-3 md:grid-cols-4 xl:grid-cols-5   gap-[1px] mt-2">
-      {posts.map((post: any) => (
-        <div key={post.id} className="max-sm:h-[155px] h-full">
-          <ImageGrid imageUrl={post!.imageUrl || ""} />
-        </div>
-      ))}
+      {posts.map((post: any) => {
+        return (
+          <div key={post.postId} className="max-sm:h-[155px] h-full">
+            <ImageGrid imageUrl={post.post.imageUrl || ""} />
+          </div>
+        );
+      })}
+    </div>
+  ) : (
+    <div className="flex flex-col gap-4 h-48 items-center mx-auto justify-center">
+      <Image
+        src={"/assets/icons/video.svg"}
+        alt="video"
+        width={64}
+        height={64}
+        className=" text-neutral-400"
+      />
+      <h2 className="text-base font-semibold text-neutral-800">
+        No post saved
+      </h2>
     </div>
   );
 };
