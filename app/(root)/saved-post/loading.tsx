fd4153696b84742d7d716c4e8d5702d0c4eaf176
purboyndra <purboindra@gmail.com>
2024-07-24 20:39:15 +0700

    add loading

---
+++ app/(root)/saved-post/loading.tsx
@@ -0,0 +1,19 @@
+import { Skeleton } from "@/components/ui/skeleton";
+import React from "react";
+
+const Loading = () => {
+  return (
+    <section>
+      <div className="flex flex-wrap gap-4">
+        {[1, 2, 3, 4, 5, 6, 7, 8, 9, 10].map((item) => (
+          <Skeleton
+            key={item}
+            className="h-60 w-full rounded-2xl sm:w-[260px]"
+          />
+        ))}
+      </div>
+    </section>
+  );
+};
+
+export default Loading;
