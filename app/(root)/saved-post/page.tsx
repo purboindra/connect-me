1fffe8d3b9c47019622b5e66786ff0d6d9b27b1b
purboyndra <purboindra@gmail.com>
2024-07-24 20:35:05 +0700

    emptycontent

--- app/(root)/saved-post/page.tsx
+++ app/(root)/saved-post/page.tsx
@@ -2,6 +2,8 @@ import { fetchSavedPostByuserId } from "@/app/actions/post.action";
 import { getCurrentUser } from "@/app/actions/user.action";
 import ImageGrid from "@/components/shared/ImageGrid";
 import React from "react";
+import Image from "next/image";
+import { EmptyContent } from "@/components/shared/EmptyContent";
 
 export default async function page() {
   const user = await getCurrentUser();
@@ -12,13 +14,20 @@ export default async function page() {
       <h1 className="text-3xl max-sm:text-lg font-bold text-neutral-800">
         Saved Post
       </h1>
-      <div className="grid grid-cols-5  max-sm:grid-cols-3 gap-2">
-        {savedPosts.map((post: any) => (
-          <div key={post.post.id} className="max-sm:h-[155px] h-full">
-            <ImageGrid imageUrl={post.post.imageUrl || ""} />
-          </div>
-        ))}
-      </div>
+      {savedPosts.length > 0 ? (
+        <div className="grid grid-cols-5  max-sm:grid-cols-3 gap-2">
+          {savedPosts.map((post: any) => (
+            <div key={post.post.id} className="max-sm:h-[155px] h-full">
+              <ImageGrid imageUrl={post.post.imageUrl || ""} />
+            </div>
+          ))}
+        </div>
+      ) : (
+        <EmptyContent
+          title={"No post saved"}
+          imageUrl={"/assets/icons/video.svg"}
+        />
+      )}
     </section>
   );
 }
