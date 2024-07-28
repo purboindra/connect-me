6ea1c1e25f69f8d65da3a605d58b3ae5fbcbb6e5
purboyndra <purboindra@gmail.com>
2024-07-28 17:31:41 +0700

    todo logic middleware

--- middleware.ts
+++ middleware.ts
@@ -1,27 +1,63 @@
 import { NextResponse, NextRequest } from "next/server";
 import { isTokenExpired } from "./lib/utils";
+import { refreshToken } from "./app/actions/auth.action";
 
 export async function middleware(request: NextRequest) {
-  const token = request.cookies.get("access_token")?.value;
+  const requestHeaders = new Headers(request.headers);
 
-  const isExpired = isTokenExpired(token || "");
+  const token = request.cookies.get("access_token");
 
   const hasOnLogin = request.url.includes("/login");
   const hasOnRegister = request.url.includes("/register");
 
-  // If the user is not authenticated or token is expired, redirect to login or register page if not already on those pages
-  if (!token || (token && isExpired)) {
-    if (!hasOnLogin && !hasOnRegister) {
-      return NextResponse.rewrite(new URL("/login", request.url));
+  const response = NextResponse.next();
+
+  if (!token && (hasOnLogin || hasOnRegister))
+    return NextResponse.rewrite(new URL("/login", request.url));
+
+  if (token) {
+    const expired = isTokenExpired(token!.value);
+    if (expired) {
+      const refresh = await refreshToken();
+
+      console.log("response refresh middlewar", refresh);
+
+      if (refresh === null) {
+        return NextResponse.rewrite(new URL("/login", request.url));
+      }
+
+      const requestHeaders = new Headers(request.headers);
+      requestHeaders.set("access_token", `${refresh.access_token}`);
+
+      const response = NextResponse.next({
+        request: {
+          headers: requestHeaders,
+        },
+      });
+
+      response.cookies.set("access_token", refresh.access_token, { path: "/" });
+      response.cookies.set("refresh_token", refresh.refresh_token, {
+        path: "/",
+      });
+
+      // Ensure the response is not cached
+      response.headers.set(
+        "Cache-Control",
+        "no-store, no-cache, must-revalidate, proxy-revalidate, max-age=0"
+      );
+
+      console.log("Updated tokens in middleware:", refresh.access_token);
+
+      return response;
     }
   }
 
   // If the user is on the login or register page and already has a valid token, redirect them to the home page or some other page
-  if ((hasOnLogin || hasOnRegister) && token && !isExpired) {
+  if ((hasOnLogin || hasOnRegister) && token) {
     return NextResponse.redirect(new URL("/", request.url));
   }
 
-  return NextResponse.next();
+  return response;
 }
 
 export const config = {
