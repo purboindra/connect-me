61599fbe13d4b295b0a8fb786d7a154353e75d19
purboyndra <purboindra@gmail.com>
2024-08-02 05:24:33 +0700

    promise all

--- app/(root)/(home)/page.tsx
+++ app/(root)/(home)/page.tsx
@@ -1,25 +1,20 @@
 import { fetchAllPost } from "@/app/actions/post.action";
-import {
-  fetchAllUser,
-  fetchFollow,
-  getCurrentUser,
-} from "@/app/actions/user.action";
+import { getCurrentUser } from "@/app/actions/user.action";
 import { FeedItem } from "@/components/shared/FeedItem";
 import Image from "next/image";
 import React from "react";
 
 export default async function page() {
-  const posts = await fetchAllPost();
-
-  const follows = await fetchFollow();
-
-  const currentUser = await getCurrentUser();
+  const [posts, currentUser] = await Promise.all([
+    fetchAllPost(),
+    getCurrentUser(),
+  ]);
 
   return (
     <section className="flex flex-col items-center">
-      <div className="w-full overflow-auto no-scrollbar">
-        <div className="flex py-4 flex-row gap-4 ">
-          {follows.map((user: any) => (
+      {/* <div className="w-full overflow-auto no-scrollbar"> */}
+      {/* <div className="flex py-4 flex-row gap-4 "> */}
+      {/* {follows.map((user: any) => (
             <div key={user.followedUserId} className="flex flex-col gap-1">
               <div className="rounded-full h-[56px] w-[56px] p-[2px] bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500">
                 <div className=" bg-neutral-300 w-full h-full rounded-full">
@@ -34,14 +29,14 @@ export default async function page() {
               </div>
               <p className="truncate w-[56px] flex">{user.follower.username}</p>
             </div>
-          ))}
-        </div>
-      </div>
+          ))} */}
+      {/* </div> */}
+      {/* </div> */}
       {/* FEED */}
       {posts.length > 0 ? (
         <FeedItem posts={posts} user={currentUser} />
       ) : (
-        <div className="flex flex-col space-y-4 mx-auto items-center justify-center">
+        <div className="flex flex-col space-y-2 mx-auto items-center justify-center">
           <h2>No posts or users found</h2>
           <Image
             src={"/assets/icons/video.svg"}
--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -9,15 +9,17 @@ export default async function Layout({
 }: {
   children: React.ReactNode;
 }) {
-  const currentUser = await getCurrentUser();
-  const users = await fetchSuggestedUser();
+  const [currentUser, users] = await Promise.all([
+    getCurrentUser(),
+    fetchSuggestedUser(),
+  ]);
 
   return (
     <main className="relative">
       <Navbar user={currentUser} />
       <div className="flex">
         <LeftSideBar user={currentUser} />
-        <section className="flex min-h-screen flex-1 flex-col px-6 pb-6 pt-24 max-md:pb-14 sm:px-8 overflow-x-auto">
+        <section className="flex min-h-screen flex-1 flex-col px-6 pb-6 pt-12 max-md:pb-14 sm:px-8 overflow-x-auto">
           <div className="max-w-5xl mx-auto w-full">{children}</div>
         </section>
         <RightSideBar users={users} />
